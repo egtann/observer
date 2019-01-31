f4e61deadad0312bc95f39fc44edca61cbcfd699
Evan Tann <egtann@gmail.com>
2019-01-31 07:48:29 -0800

    add start, end event recording methods

--- log.go
+++ log.go
@@ -1,11 +1,23 @@
 package observer
 
-type LogKey string
-
 const (
-	LogKeyRole      LogKey = "role"
-	LogKeyHost             = "host"
-	LogKeyReqID            = "request_id"
-	LogKeyTimestamp        = "ts"
-	LogKeyMsg              = "msg"
+	LogKeyRole      = "role"
+	LogKeyHost      = "host"
+	LogKeyReqID     = "req_id"
+	LogKeyTimestamp = "ts"
+	LogKeyMsg       = "msg"
 )
+
+type Logger interface {
+	Printf(msg string, vals ...interface{})
+}
+
+// Start recording an event.
+func Start(l Logger, event string) {
+	l.Printf("start: %s", event)
+}
+
+// End event recording.
+func End(l Logger, event string) {
+	l.Printf("end: %s", event)
+}
